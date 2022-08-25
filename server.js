311ce862c4d00fb5a4f2e05d263a12f3e7ede346
diegobenitez1204 <dyeben86@gmail.com>
2022-08-25 01:07:08 -0300

    actualizando

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(cors())
 app.use(express.json()); 
 
-app.get('/', (req, res)=> { res.send("trabajando ") })
+app.get('/', (req, res)=> { res.send("trabajando de noche ") })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
